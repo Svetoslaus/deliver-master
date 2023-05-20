7d3a9f7782511ad0699cd999127d3d4c58d116e9
svetoslav <svetoslav.shterev@monday-consulting.com>
2023-05-21 00:51:22 +0200

    deliver_02: save data in DB

--- jsondb/produkte.js
+++ jsondb/produkte.js
@@ -6,7 +6,13 @@ const jsondb = {
             kategorie: 'Soft Drinks',
             preis: 3.99,
             url: 'cans',
-            bild: '/bilder/produkte/drinks.jpg'
+            bild: '/bilder/produkte/drinks.jpg',
+            extras: [
+                {
+                    text: 'gekühlt',
+                    preis: 0.3,
+                }
+            ]   
         },
         {
             name: 'ice-cream',
@@ -14,7 +20,13 @@ const jsondb = {
             kategorie: 'Dessert',
             preis: 4.99,
             url: 'ice',
-            bild: '/bilder/produkte/ice.jpg'
+            bild: '/bilder/produkte/ice.jpg',
+            extras: [
+                {
+                    text: 'choko Sirup',
+                    preis: 0.8,
+                }
+            ]
         },
         {
             name: 'muffin',
@@ -22,7 +34,13 @@ const jsondb = {
             kategorie: 'Dessert',
             preis: 1.99,
             url: 'muffin',
-            bild: '/bilder/produkte/muffin.jpg'
+            bild: '/bilder/produkte/muffin.jpg',
+            extras: [
+                {
+                    text: 'extra Sahne',
+                    preis: 1,
+                }
+            ]
         },
         {
             name: 'pommes',
@@ -30,7 +48,17 @@ const jsondb = {
             kategorie: 'Meal',
             preis: 1.99,
             url: 'pommes',
-            bild: '/bilder/produkte/pommes.jpg'
+            bild: '/bilder/produkte/pommes.jpg',
+            extras: [
+                {
+                    text: 'doppelt',
+                    preis: 4,
+                },
+                {
+                    text: 'extra scharf',
+                    preis: 0.6
+                }
+            ]
         },
         {
             name: 'pizza',
@@ -38,7 +66,17 @@ const jsondb = {
             kategorie: 'Meal',
             preis: 4.99,
             url: 'pizza',
-            bild: '/bilder/produkte/pizza.jpg'
+            bild: '/bilder/produkte/pizza.jpg',
+            extras: [
+                {
+                    text: 'doppelt',
+                    preis: 4,
+                },
+                {
+                    text: 'extra scharf',
+                    preis: 0.6
+                }
+            ]
         },
         {
             name: 'spaghetti',
@@ -46,7 +84,17 @@ const jsondb = {
             kategorie: 'Meal',
             preis: 3.99,
             url: 'spaghetti',
-            bild: '/bilder/produkte/spaghetti.jpg'
+            bild: '/bilder/produkte/spaghetti.jpg',
+            extras: [
+                {
+                    text: 'doppelt',
+                    preis: 4,
+                },
+                {
+                    text: 'extra scharf',
+                    preis: 0.6,
+                }
+            ]
         },
         {
             name: 'fish',
@@ -54,7 +102,13 @@ const jsondb = {
             kategorie: 'Meal',
             preis: 5.99,
             url: 'fish',
-            bild: '/bilder/produkte/fish.jpg'
+            bild: '/bilder/produkte/fish.jpg',
+            extras: [
+                {
+                    text: 'extra Pommes',
+                    preis: 2,
+                }
+            ]
         },
         
     ]
